0b3367c10681863c32a70e29fad02a82f050b7b0
kratikabajpai15 <kratikabajpai621@gmail.com>
2026-04-24 00:04:47 +0530

    Switch to Iris dataset and update SVM model evaluation

--- SupportVectorMachine.py
+++ SupportVectorMachine.py
@@ -1,9 +1,22 @@
-from sklearn.datasets import load_breast_cancer
+from sklearn import svm
+from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVC
-data = load_breast_cancer()
+from sklearn.metrics import accuracy_score
+
+data = load_iris()
 X = data.data
 y = data.target
+
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+
+model = svm.SVC()
+
+
 model.fit(X_train, y_train)
-print("Accuracy:", model.score(X_test, y_test))
+
+y_pred = model.predict(X_test)
+
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
